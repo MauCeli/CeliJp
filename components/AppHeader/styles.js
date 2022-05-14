7ee1e0b6799bf0d2b093e902c7553e28e7337909
Celi <vmceli64@fi.unju.edu.ar>
2022-05-14 03:48:48 -0300

    ultima mejora en el header

--- components/AppHeader/styles.js
+++ components/AppHeader/styles.js
@@ -7,17 +7,19 @@ export const globalStyles = css.global``;
 //estilos locales Applayout
 export default css`
   .header {
-
+  background: ${color.primary};
   width: 100%;
   height: 23%;
   font-weight: bold;
   text-align: center;
-  background: red;
+  padding:0.3rem;
+  color:white;
   }
 
 
 .header_logo {
-  padding-top: 10px;
+  padding-top: 0px;
+  margin:0;
   font-size: 50px;
   text-shadow: 3px 4px rgba(black, .1);
   transition: .3s;
